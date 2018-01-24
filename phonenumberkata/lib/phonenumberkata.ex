740093414446ceee5eb45dfbde00f768966c6e29
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2018-01-24 18:15:51 +1300

    trying to implement drop name in main

--- phonenumberkata/lib/phonenumberkata.ex
+++ phonenumberkata/lib/phonenumberkata.ex
@@ -2,12 +2,14 @@ defmodule MAIN do
   def main do
     getFile()
     |> convertToMaps()
+    # |> Enum.each(&dropName(&1))
+    # |> Enum.take(1)
 
   end
 
   def getFile do
     File.stream!("phone_data.csv") 
-    |> CSV.decode!(headers: true)
+    |> CSV.decode(headers: true)
   end
 
   def convertToMaps(stream) do
@@ -17,14 +19,7 @@ defmodule MAIN do
   def dropName(map) do
     Map.drop(map, ["Name"])
   end
-
-  # def clean(stream) do
-  #   Stream.map([stream], fn(x) -> removeName(x) end)
-  #   |> Enum.take(1)
-  # end
-
-  # def removeName(ok: %{"Name" => val1, "Phone Number" => val2}) do
-  #   Map.drop(%{"Name" => val1, "Phone Number" => val2}, [:Name])
-  # end
+  end
+  
 
 end
