4b159d0a745212aaa958242cdc21211a8e3ed147
Nick Ye <nick.ye888@gmail.com>
2018-01-24 23:10:32 +1300

    WIP clean number

--- phonenumberkata/lib/phonenumberkata.ex
+++ phonenumberkata/lib/phonenumberkata.ex
@@ -2,24 +2,23 @@ defmodule MAIN do
   def main do
     getFile()
     |> convertToMaps()
-    # |> Enum.each(&dropName(&1))
-    # |> Enum.take(1)
-
   end
 
   def getFile do
     File.stream!("phone_data.csv") 
-    |> CSV.decode(headers: true)
+    |> CSV.decode!(headers: true)
   end
 
   def convertToMaps(stream) do
     Enum.to_list(stream)
   end
 
-  def dropName(map) do
-    Map.drop(map, ["Name"])
+  # def dropName(map) do
+  #   Map.drop(map, ["Name"])
+  # end
+
+  def cleanPhoneNumber(map) do
+   Map.get_and_update(map, "Phone Number", fun) #todo
   end
-  end
-  
 
 end
--- phonenumberkata/test/phonenumberkata_test.exs
+++ phonenumberkata/test/phonenumberkata_test.exs
@@ -10,10 +10,16 @@ test "get the contents of file and convert to list of maps" do
         
 end
 
-test "should remove name from map" do
+# test "should remove name from map" do
+#         input = %{"Name"  => "Kimberlee Turlington", "Phone Number" => "039 298-72-30"}
+#         expected = %{"Phone Number" => "039 298-72-30"}
+#         assert MAIN.dropName(input) == expected
+# end
+
+test "should remove spaces and symbols from phone number" do
         input = %{"Name"  => "Kimberlee Turlington", "Phone Number" => "039 298-72-30"}
-        expected = %{"Phone Number" => "039 298-72-30"}
-        assert MAIN.dropName(input) == expected
+        expected = %{"Name"  => "Kimberlee Turlington", "Phone Number" => "0392987230"}
+        assert MAIN.cleanPhoneNumber(input) == expected
 end
 
 end
