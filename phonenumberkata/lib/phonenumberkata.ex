34160ca7546fad870d463a34761f325187cc3c4f
Nick Ye <30612557+nickyemyob@users.noreply.github.com>
2018-01-24 15:16:09 +1300

    converted file to list

--- phonenumberkata/lib/phonenumberkata.ex
+++ phonenumberkata/lib/phonenumberkata.ex
@@ -1,22 +1,34 @@
 defmodule MAIN do
   def main do
     getFile()
-    |> clean()
+    |> convertFileToList()
+    |> dropName()
+
   end
 
   def getFile do
     File.stream!("phone_data.csv") 
-    |> CSV.decode(headers: true)  
+    |> CSV.decode
+  end
+
+  def convertFileToList(stream) do
+    Enum.to_list(stream)
+  end
+
+# %{"Name" => a, "Phone Number" => b}
+  def dropName(list) do
+    Enum.each(list, Map.drop(%{"Name": a, "Phone Number": b}, [:Name])
     
   end
 
-  def clean(stream) do
-    Stream.map([stream], fn(x) -> removeName(x) end)
-    |> Enum.take(1)
-  end
 
-  def removeName(ok: %{"Name" => val1, "Phone Number" => val2}) do
-    Map.drop(%{"Name" => val1, "Phone Number" => val2}, [:Name])
-  end
+  # def clean(stream) do
+  #   Stream.map([stream], fn(x) -> removeName(x) end)
+  #   |> Enum.take(1)
+  # end
+
+  # def removeName(ok: %{"Name" => val1, "Phone Number" => val2}) do
+  #   Map.drop(%{"Name" => val1, "Phone Number" => val2}, [:Name])
+  # end
 
 end
--- phonenumberkata/test/phonenumberkata_test.exs
+++ phonenumberkata/test/phonenumberkata_test.exs
@@ -2,28 +2,22 @@ defmodule MAINTest do
   use ExUnit.Case
   doctest MAIN
 
-  test "should be consistant when a phone number is not the start of another phone number" do
-    input = "Name,Phone Number" + '\n'
-            "Kimberlee Turlington,039 298-72-30" + '\n'
-            "Miguel Eveland,032-2659094" + '\n'
-            "Audrie Smid,03758295-10 32" + '\n'
-            "Antonia Maslanka,00594-83-34 93"
-    expected = true
-    assert MAIN.isConsistant() == expected
-  end
+#   test "should be consistant when a phone number is not the start of another phone number" do
+#     input = "Name,Phone Number" + '\n'
+#             "Kimberlee Turlington,039 298-72-30" + '\n'
+#             "Miguel Eveland,032-2659094" + '\n'
+#             "Audrie Smid,03758295-10 32" + '\n'
+#             "Antonia Maslanka,00594-83-34 93"
+#     expected = true
+#     assert MAIN.isConsistant() == expected
+#   end
 
-  test "should remove names and heading from data" do
-    input = "Name,Phone Number" + '\n'
-            "Kimberlee Turlington,039 298-72-30" + '\n'
-            "Miguel Eveland,032-2659094" + '\n'
-            "Audrie Smid,03758295-10 32" + '\n'
-            "Antonia Maslanka,00594-83-34 93"
+  test "should remove name from map"
+        input = %{"Name"  => "", "Phone Number"  => ""}
+        expected = %{"Phone Number": ""}
+        assert MAIN.dropName(input) == expected
 
-    expected = "0392987230" + '\n'
-            "0322659094" + '\n'
-            "037582951032" + '\n'
-            "00594833493"
 
-    assert MAIN.clean() == expected
+
 
 end
